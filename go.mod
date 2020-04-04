22273fb7273db7deb20d331c0d3597ba823e4752
nyo <nyo4rdace@gmail.com>
2020-04-04 23:21:33 +0800

    add mysql and mongodb connection test

--- go.mod
+++ go.mod
@@ -5,6 +5,9 @@ go 1.14
 require (
 	github.com/fatih/color v1.9.0
 	github.com/go-redis/redis v6.15.7+incompatible
+	github.com/go-sql-driver/mysql v1.5.0
 	github.com/onsi/ginkgo v1.12.0 // indirect
 	github.com/onsi/gomega v1.9.0 // indirect
+	go.mongodb.org/mongo-driver v1.3.1
+	gopkg.in/yaml.v2 v2.2.8
 )
--- main.go
+++ main.go
@@ -1,19 +1,67 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"io/ioutil"
 	"nebulosa-studio/quicktest/status"
+	"time"
 
 	"github.com/go-redis/redis"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"gopkg.in/yaml.v2"
+
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	redisTest("localhost", "6379")
+	targets, err := read("test.yml")
+	if err != nil {
+		fmt.Println("can not file yaml file")
+		return
+	}
+
+	for _, i := range targets {
+		switch i.Type {
+		case "redis":
+			redisTest(i)
+		case "mysql":
+			mysqlTest(i)
+		case "mongodb":
+			mongodbTest(i)
+		default:
+			fmt.Println("unknown type:", i.Type, i)
+		}
+	}
 }
 
-func redisTest(host, port string) {
+// Target of testing
+type Target struct {
+	Name     string
+	Type     string
+	Host     string
+	Port     string
+	Username string
+	Password string
+}
+
+func read(file string) ([]Target, error) {
+	m := []Target{}
+	data, err := ioutil.ReadFile(file)
+	if err != nil {
+		return m, err
+	}
+	err = yaml.Unmarshal(data, &m)
+	return m, err
+}
+
+func redisTest(t Target) {
 	client := redis.NewClient(&redis.Options{
-		Addr: fmt.Sprintf("%s:%s", host, port),
+		Addr: fmt.Sprintf("%s:%s", t.Host, t.Port),
 	})
 	r, err := client.Ping().Result()
 	if err != nil {
@@ -22,4 +70,39 @@ func redisTest(host, port string) {
 	}
 
 	status.Print("redis", status.Success, r)
+}
+
+func mysqlTest(t Target) {
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s)/", t.Username, t.Password, t.Host))
+	if err != nil {
+		status.Print("mysql", status.Error, err.Error())
+		return
+	}
+
+	err = db.Ping()
+	if err != nil {
+		status.Print("mysql", status.Error, err.Error())
+		return
+	}
+
+	status.Print("mysql", status.Success, "ping")
+}
+
+func mongodbTest(t Target) {
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(fmt.Sprintf("mongodb://%s:%s", t.Host, t.Port)))
+	if err != nil {
+		status.Print("mongodb", status.Error, err.Error())
+		return
+	}
+	defer client.Disconnect(ctx)
+
+	ctx, _ = context.WithTimeout(context.Background(), 2*time.Second)
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		status.Print("mongodb", status.Error, err.Error())
+		return
+	}
+
+	status.Print("mongodb", status.Success, "pong")
 }
